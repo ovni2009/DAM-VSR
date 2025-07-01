cee9598b9488e984313b65694567fcf11ce46761
Zhe Kong <43921423+kongzhecn@users.noreply.github.com>
2025-07-01 21:35:10 +0800

    Add files via upload

---
+++ SUPIR/CKPT_PTH.py
@@ -0,0 +1,4 @@
+LLAVA_CLIP_PATH = 'checkpoints/clip-vit-large-patch14-336'
+LLAVA_MODEL_PATH = 'checkpoints/llava-v1.5-13b'
+SDXL_CLIP1_PATH = 'checkpoints/clip-vit-large-patch14'
+SDXL_CLIP2_CKPT_PTH = 'checkpoints/CLIP-ViT-bigG-14-laion2B-39B-b160k/open_clip_pytorch_model.bin'
